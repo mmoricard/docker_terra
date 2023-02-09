9b2d99a92849b7c12fa168f523076a7de48e881d
josuecodjo <49247887+josuecodjo@users.noreply.github.com>
2023-02-09 11:13:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "docker_image" "nginx" {
 
 # start a container and expose the 30100 port
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
+  image = docker_image.nginx.image_id
   name  = var.nginx_container_name
   ports {
     internal = 80
