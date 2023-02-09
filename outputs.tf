65bbc80ea2b7fe563f7481155007567111eda1e4
josuecodjo <49247887+josuecodjo@users.noreply.github.com>
2023-02-09 11:13:52 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,9 @@ output "docker_container_name" {
   description = "name of the docker container"
   value       = docker_container.nginx.name
 }
+
+output "docker_container_port" {
+  description = "exposed port of the docker container"
+  value       = docker_container.nginx.ports[0].external
+}
+
